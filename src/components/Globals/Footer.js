2b0a9f182fc1e014ef085e1f08c7babc85ab507f
bdmllc <beachdaymedia@gmail.com>
2019-12-27 08:02:35 -0500

    changed colors on submit button and footer

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -56,7 +56,7 @@ export default class Footer extends Component {
 
 const FooterWrapper = styled.footer`
   padding: 1rem 0;
-  background: black;
+  background: #487e7e;
   .icons {
     width: 10rem;
     display: flex;
